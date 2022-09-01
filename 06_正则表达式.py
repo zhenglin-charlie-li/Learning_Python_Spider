2c6f3a8d857a14a2b91c255c9b0c21fc85976c1b
Zhenglin Li <1125806272@qq.com>
2022-09-01 15:31:40 +0800

    delete

--- 06_正则表达式.py
+++
@@ -1,8 +0,0 @@
-import re
-
-rs = re.findall('\d','123')
-rs = re.findall('\d*','456')
-rs = re.findall('\d+','789')
-rs = re.findall('a+','aaabcd')
-
-print(rs)
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get('https://file1.dxycdn.com/2020/0315/553/3402160512808052518-135.json')
-print(response.content.decode())
